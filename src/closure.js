9dc1e9c1147985d9418fbe8a99739967f5c78f7f
Sergey80 <ses.box@gmail.com>
2014-07-21 11:01:01 -0400

    function & closure

--- src/closure.js
+++ src/closure.js
@@ -1,12 +1,12 @@
 // #closure
 
-console.log("\n:" + 1);
+console.log("\n:1 ----");
 
-// 1. closure - is just a local copy of global var in some function
+// 1. closure - is is a place where "var" is stored for the functions which has access to it
 function fooX(x) {
     var global = 1;
     function fooY(y) {
-        global = global + 2;  // 1 + 2 = 3
+        global = global + 2;  // 1 + 2 = 3                  // it creates a closure and put new new 'global' var
         // creates a copy of 'global' for "fooY" function
         console.log(x + y + global); // 1 + 2 + 3 = 6
     }
@@ -15,51 +15,33 @@ function fooX(x) {
 fooX(1); // 6
 fooX(1); // still 6! but NOT 8,10, +2 etc.
 
-// 2. -- does copy the "var" at them moment of calling a function
-console.log("\n:" + 2);
 
-function say2() {
-    var num = 1;
-    var doLog = function() {console.log(num);};
-    num =  num + 1;
-    return doLog;
-}
 
-var doLog = say2();
-console.log(doLog); // [Function]
-doLog(); // =2,
-         // it does copy of "num"
-         // _at the moment_ of invoking the function say2().doLog()
+// 2. closure is created not for one function only - where more than one function may access to
+console.log("\n:2 ----");
 
-// 3. -- what if many function access the same "var"
-console.log("\n:" + 3);
-
-function many() {
+function scope() {
     var num = 1;    // num is common for evey function
-    one =   function() { num = num + 1; console.log( "one: " + num )};
-    two =   function() { num = num + 1; console.log( "two: " + num )};
+    one =   function() { num = num + 1; console.log( "one: " + num )};  // put 'num' to the closure
+    two =   function() { num = num + 1; console.log( "two: " + num )};  // 'num' is already in the closure
     // "one" and "two" are all in the one scope in regard to "num"-closure
 }
 
-many(); // setup global variables
+scope(); // setup global variables
 
-one();  // "2"     | one
-two();      // "3" | and "two" uses same "shared" "num" "closure" variable
-// that is quite dangerous - we should NOT expect that there is one copy to one function!
-// so, closure in JS is created not one per function, but one per function scope..if you will
+one();  // "2"     | creates a closure    /
+two();  // "3"     | use existing closure /
 
-// 4. -- List of function in an array & for-loop.
-//    -- That is interesting ! Even more unexpected
+// 3. -- it is common way to come with mistake
 
-console.log("\n:4");
+console.log("\n:3 ----");
 
 function buildList(list) {
     var result = [];
     for (var i=0; i< list.length; i++) {
         var item = 'item' + list[i];
         result.push( function() {
-            // i = 2  // if uncomment - then it will take a copy of i for that function
-            console.log(item + ' ' + list[i]); // "i" will be = 3 in any time !
+            console.log(item + ' ' + list[i]); // i = 3 !
         });
     }
     return result;  // returns list of functions
@@ -67,10 +49,30 @@ function buildList(list) {
 
 function testList() {
     var fnList = buildList([1,2,3]);    //
-    for (var i=0; i<fnList.length; i++) {
-        fnList[i](); // calling each function, one after another
-            // in the moment of calling the function, "i" already = 3
+    for (var j=0; j<fnList.length; j++) {
+        fnList[j]();   // in the moment of calling the function, "i" already = 3  !
     }
 }
 
 testList();  // expect "undefined" in output. since list[3] is out of the array
+
+console.log(":4 ----");
+
+// 4 - common use case:
+function newClosure(someNum, someSettings) {
+
+    var num = someNum;
+    var anArray = [1,2,3];
+    var settings = someSettings;
+
+    return function(x) {
+        num = num + x;      // creates a closure and puts 'num' to it
+        anArray.push(num);
+        console.log('num: ' + num + '\nanArray: ' + anArray.toString() + '\nsettings.someVar: ' + settings.someVar);
+    }
+}
+var objSettings = {someVar: 4}; // this object will not be changes
+console.log("> first call");
+newClosure(4, objSettings)(1);  // num: 5; anArray: 1,2,3,5; ref.someVar: 4;
+console.log("> second call");
+newClosure(4, objSettings)(1);  // num: 5; anArray: 1,2,3,5; ref.someVar: 4;
